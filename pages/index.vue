705a49450075b728c8904c8cee8156639f16db99
Emilija-S <emilija.simonoska@hotmail.com>
2022-09-13 16:25:31 +0200

    Created select form + connected with backend through post method

--- pages/index.vue
+++ pages/index.vue
@@ -2,74 +2,41 @@
   <main>
     <section class="bg-light">
       <div class="container d-flex flex-column align-items-center py-5">
-        <h1 class="fw-bold mb-5 text-center">USER</h1>
+        <h1 class="fw-bold mb-5 text-center">USER-PERMISSION RELATION</h1>
         <div class="row d-flex justify-content-center mt-5">
           <div class="col-10 col-lg-8">
             <form
-              id="member-form"
+              id="user-permission-form"
               role="form"
               class="ui form"
               @submit.prevent="sendUserPermissions"
             >
               <div class="row">
-                <div class="col col-12 col-lg-6">
-                  <label for="name">Name</label>
-                  <input
-                    data-bs-toggle="tooltip"
-                    title="please fill out this field"
-                    id="name"
-                    name="name"
-                    v-model="form.name"
-                    type="text"
-                    class="form-control bg-light border border-2 rounded-0 px-0 mb-5"
-                  />
+                <select class="form-select col col-6 mt-3 mb-5"
+                        id="permission_id"
+                        name="permission_id"
+                        v-model="form.permission_id"
+                >
+                  <option selected>Select the permission</option>
+                  <option v-for="permission in permissions" :value="permission.id">{{ permission.name }}</option>
+                </select>
+                <select class="form-select col col-6 mt-3 mb-5"
+                        id="user_id"
+                        name="user_id"
+                        v-model="form.user_id"
+                >
+                  <option selected>Select the user</option>
+                  <option v-for="user in users" :value="user.id">{{ user.name }}</option>
+                </select>
+                <div class="d-flex justify-content-end">
+                  <button @click="sendUserPermissions" type="submit"
+                          class="ui button btn btn-outline-dark rounded-pill px-4 ">
+                    SUBMIT
+                  </button>
                 </div>
-                <div class="col col-12 col-lg-6">
-                  <label for="email">Email</label>
-                  <input
-                    data-bs-toggle="tooltip"
-                    title="please fill out this field"
-                    id="email"
-                    name="email"
-                    v-model="form.email"
-                    type="email"
-                    class="form-control bg-light border border-2 rounded-0 px-0 mb-5"
-                  />
-                </div>
-                <div class="col col-12">
-                  <label for="password">Password</label>
-                  <input
-                    data-bs-toggle="tooltip"
-                    title="please fill out this field"
-                    id="password"
-                    name="password"
-                    v-model="form.password"
-                    type="password"
-                    class="form-control bg-light border border-2 rounded-0 px-0 mb-5"
-                  />
-                </div>
-              </div>
-              <select class="form-select form-select-sm col col-12 mt-3 mb-5"
-                      multiple aria-label="multiple select example"
-                      id="permissions"
-                      name="permissions[]"
-                      v-model="form.permissions"
-              >
-                <option value="1">READ</option>
-                <option value="2">WRITE</option>
-                <option value="3">EXECUTE</option>
-              </select>
-              <div class="d-flex justify-content-end">
-                <button @click="sendUserPermissions" type="submit"
-                        class="ui button btn btn-outline-dark rounded-pill px-4 ">
-                  SUBMIT
-                </button>
               </div>
             </form>
           </div>
-        </div>
-        <div class="my-5">
-          {{ users }}
         </div>
       </div>
     </section>
@@ -82,12 +49,11 @@ export default {
   data() {
     return {
       form: {
-        name: "",
-        email: "",
-        password: "",
-        permissions: [],
+        permission_id: "",
+        user_id: "",
       },
-      users: ""
+      users: [],
+      permissions: [],
     };
   },
   created() {
@@ -98,14 +64,22 @@ export default {
       .catch((error) => {
         console.log(error);
       });
+    this.$axios.get('/api/all-permissions')
+      .then((response) => {
+        this.permissions = JSON.parse(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
   methods: {
-    async sendUserPermissions() {
+      async sendUserPermissions() {
       try {
-        await this.$axios.post('/api/contact-form', this.form);
+        await this.$axios.post('/api/user-permission', this.form);
       } catch (error) {
         console.log(error);
       } finally {
+        console.log(this.form);
       }
     },
   }
